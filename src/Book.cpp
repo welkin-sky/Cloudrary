1f4301989f0919ec7a57ee71ee2cdcefe26a0791
lingkerio <lingkerio@qq.com>
2023-05-19 00:42:10 +0800

    little modification

--- src/Book.cpp
+++ src/Book.cpp
@@ -2,5 +2,19 @@
 // Created by Welkinsky on 5/18.
 //
 
+#include <string>
 #include "../include/base.h"
+
+Book::Book(const string& j) {
+  
+}
+
+string Book::serialize(Book& b) {
+
+}
+
+Book& Book::deserialize(string j) {
+
+}
+
 
